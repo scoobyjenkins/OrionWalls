a18901d2a0a4f5efb8595859ca3cedaf4b21d2ca
Yeshwanth V Shenoy <yeshwanthvshenoy@gmail.com>
2015-09-22 07:59:06 +0530

    Revamp Walls App

--- Android.mk
+++ Android.mk
@@ -1,11 +1,26 @@
-LOCAL_PATH:= $(call my-dir)
+#
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := OrionWalls
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
-LOCAL_RESOURCE_DIR := packages/apps/OrionWalls/app/src/main/res
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := OrionWalls
 
 include $(BUILD_PACKAGE)
